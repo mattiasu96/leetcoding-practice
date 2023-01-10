a2bd2576f3cdf3cd886f63bd9502b934d4243c13
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2023-01-10 22:11:07 +0100

    Added remove duplicates leet code

---
+++ Neet Code Course/Static Arrays/26. Remove duplicates from Sorted Array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> list[int]:
+        current_unique_value = nums[0]
+        index_update = 0
+
+        for iterate_index in range(len(nums)):
+            if nums[iterate_index] != current_unique_value:
+                index_update = index_update + 1
+                nums[index_update] = nums[iterate_index]
+                current_unique_value = nums[iterate_index]
+
+        return index_update + 1
+
+
+test = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+solution = Solution()
+result = solution.removeDuplicates(test)
+print(result)
