5d49b870f74822a0a41f8d71b46012c80272fa98
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2023-01-17 22:06:32 +0100

    baseball game dumb logic

---
+++ Neet Code Course/Static Arrays/682 baseball game.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calPoints(self, operations: list[str]) -> int:
+        record = []
+
+        for command in operations:
+
+            try:
+                number = int(command)
+                record.append(number)
+            except:
+                pass
+
+            if command == "+":
+                record.append(record[-1] + record[-2])  # farlo pythonic
+
+            if command == "D":
+                record.append(2 * record[-1])  # farlo pythonic
+
+            if command == "C":
+                record.pop()
+
+        return sum(record)
+
+
+test = ["1", "2", "3", "+", "C", "D"]
+
+solution = Solution()
+result = solution.calPoints(test)
+print(result)
