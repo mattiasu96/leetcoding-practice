7e51990d67ee5bcb82ad32a77ef8b9c7a9455433
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-04-03 09:39:23 +0200

    Added k_th value in bst

---
+++ tests/test_k_th_smallest_value_bst/test_k_th_smallest_value_bst.py
@@ -0,0 +1,43 @@
+from random_problems.k_th_smallest_value_bst.k_th_smallest_value_bst import (
+    TreeNode,
+    Solution,
+)
+
+
+def test_first_smallest_value_root():
+    root_tree = TreeNode(
+        val=10,
+        left=TreeNode(
+            val=5,
+            left=TreeNode(
+                val=1, left=None, right=TreeNode(val=3, left=None, right=None)
+            ),
+            right=None,
+        ),
+        right=None,
+    )
+
+    reference = 10
+
+    result = Solution().kthSmallest(root=root_tree, k=1)
+
+    assert result == reference
+
+def test_smallest_in_left_only_tree():
+    root_tree = TreeNode(
+        val=10,
+        left=TreeNode(
+            val=5,
+            left=TreeNode(
+                val=1, left=None, right=None
+            ),
+            right=None,
+        ),
+        right=None,
+    )
+
+    reference = 5
+
+    result = Solution().kthSmallest(root=root_tree, k=2)
+
+    assert result == reference
