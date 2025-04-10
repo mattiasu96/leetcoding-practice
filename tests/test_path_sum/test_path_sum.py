014d8868754119b5179af902b83c613cd599e591
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-04-10 10:06:02 +0200

    path_sum

---
+++ tests/test_path_sum/test_path_sum.py
@@ -0,0 +1,14 @@
+from random_problems.path_sum.path_sum import TreeNode, Solution
+
+
+
+def test_sum_is_equal_simple_tree():
+    root_tree = TreeNode(
+        val=10,
+        left=TreeNode(val=5, left=None, right=None),
+        right=TreeNode(val=12, left=None, right=None),
+    )
+
+    result = Solution().hasPathSum(root=root_tree, targetSum=15)
+
+    assert result == True
