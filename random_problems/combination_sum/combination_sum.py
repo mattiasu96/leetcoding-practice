a95279d647c15ffb6450624e00f2356e016edc7c
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-04-11 10:57:34 +0200

    added combination sum

---
+++ random_problems/combination_sum/combination_sum.py
@@ -0,0 +1,25 @@
+
+
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+
+        res = []
+        subset = []
+
+        def dfs(i):
+            
+            if sum(subset) == target:
+                res.append(subset.copy())
+                return
+            
+            if i >= len(candidates) or sum(subset) > target:
+                return
+            
+            subset.append(candidates[i])
+            dfs(i)
+
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
