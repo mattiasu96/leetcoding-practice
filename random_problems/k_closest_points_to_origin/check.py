0101df7ff234770cbd936b112f5b669b5d92759c
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-04-14 15:44:17 +0200

    xclosest point to origin

---
+++ random_problems/k_closest_points_to_origin/check.py
@@ -0,0 +1,5 @@
+from random_problems.k_closest_points_to_origin.k_closest_points_to_origin import Solution
+
+solution = Solution()
+
+print(solution._calculate_distance(10, 10, 0, 0))
---
+++ random_problems/k_closest_points_to_origin/k_closest_points_to_origin.py
@@ -0,0 +1,26 @@
+import heapq
+from math import sqrt
+
+
+class Solution:
+    def _calculate_distance(self, x_1, y_1, x_2, y_2):
+        return sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
+
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        min_heap = [
+            (self._calculate_distance(point[0], point[1], 0, 0), point)
+            for point in points
+        ]
+
+        heapq.heapify(min_heap)
+        res = []
+        while k > 0:
+            _, point = heapq.heappop(min_heap)
+            res.append(point)
+            k = k - 1
+
+        return res
+
+solution = Solution()
+
+print(solution.kClosest([[1,3],[-2,2]], 1))
