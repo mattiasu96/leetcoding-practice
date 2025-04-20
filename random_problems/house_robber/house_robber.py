1bbf8b1223a1fe5faf21dd9369db364185a3b152
mattiasu96 <37300216+mattiasu96@users.noreply.github.com>
2025-04-20 22:29:42 +0200

    house robber

---
+++ random_problems/house_robber/house_robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        cache = {}
+
+        def dfs(i):
+            
+            if i > len(nums) - 1:
+                return 0
+            
+            if i in cache:
+                return cache[i]
+
+            cache[i] = max(nums[i] + dfs(i + 2), dfs(i + 1))
+            return cache[i]
+        
+        return dfs(0)
